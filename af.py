9ac2f043fc01ce9c0d9bb96e5cb32c0a595429a0
md kausar <mdkausar246810@gmail.com>
2020-12-09 22:23:43 +0600

    AdminFinder

---
+++ af.py
@@ -0,0 +1,45 @@
+###########################################
+#WAHYU ARIF P - IDSYSTEM404
+#UPDATE : 09 DESEMBER 2020 - 9:22 PM
+###########################################
+#MD KAUSAR
+#INSTALL PYTHON
+#RUN CLI : python af.py
+###########################################
+
+from urllib2 import Request, urlopen, URLError, HTTPError
+
+def Space(j):
+	i = 0
+	while i<=j:
+		print " ",
+		i+=1
+
+
+def findAdmin():
+	f = open("admin.txt","r"); #LIST DORK ADMIN
+	link = raw_input("Masukkan URL Website : ") #MASUKKAN ALAMAT URL WEBSITE
+	print "\n\nAvilable links : \n"
+	while True:
+		sub_link = f.readline()
+		if not sub_link:
+			break
+		req_link = "http://"+link+"/"+sub_link
+		req = Request(req_link)
+		try:
+			response = urlopen(req)
+		except HTTPError as e:
+			continue
+		except URLError as e:
+			continue
+		else:
+			print "OK => ",req_link
+
+def Credit():
+	Space(9); print "##################################"
+	Space(9); print "#   +++ Admin Panel Finder +++   #"
+	Space(9); print "#     		IDSYSTEM404 	      #"
+	Space(9); print "##################################"
+
+Credit()
+findAdmin()
